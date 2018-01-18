f6552ca17a038e4899173152c03a8c0e5d779aa5
Heroqu <hero.qu@gmail.com>
2018-01-18 11:45:19 +0300

    add locale reducer

---
+++ src/config/index.js
@@ -0,0 +1,4 @@
+export const DEFAULT_LOCALE = 'en'
+export const ALLOWED_LOCALES = ['en', 'ru']
+
+export default { DEFAULT_LOCALE, ALLOWED_LOCALES }
---
+++ src/reducers/locale.js
@@ -0,0 +1,27 @@
+import { SET_LOCALE } from '../constants/actionTypes'
+import  { DEFAULT_LOCALE, ALLOWED_LOCALES } from '../config'
+
+const initialState = {
+  locale: DEFAULT_LOCALE
+}
+
+const sanitizedLocale = locale => {
+  locale = ('' + locale).toLowerCase().trim()
+  if (ALLOWED_LOCALES.indexOf(locale) !== -1) {
+    return locale
+  }
+}
+
+const localeReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_LOCALE:
+      const locale = sanitizedLocale(action.payload)
+      if (locale) {
+        return { ...state, locale }
+      }
+    default:
+  }
+  return state
+}
+
+export default localeReducer
