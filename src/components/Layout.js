67cdda5da45614ae66ff6d69c7c9698e8e562989
Heroqu <hero.qu@gmail.com>
2018-05-23 20:06:04 +0300

    clean up

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import Header from '../components/Header'
-import Content from '../components/Content'
-import Footer from '../components/Footer'
+import Header from './Header'
+import Content from './Content'
+import Footer from './Footer'
 import Debug from './Debug'
 
 const Layout = props => {
