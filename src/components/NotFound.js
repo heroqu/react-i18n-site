b3b1e4f110721dea612ee64296017525f3ce690a
Heroqu <hero.qu@gmail.com>
2018-07-14 15:16:18 +0300

    NotFound component is now fully i18n-ed

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -13,7 +13,13 @@ const NotFound = props => (
       />
     </h1>
     <h4>
-      You can go to the <AppLink to={`/${HOME_PAGE}`}>homepage</AppLink>
+      <FormattedMessage
+        id="app.you_can_go_to_the"
+        defaultMessage="You can go to the"
+      />{' '}
+      <AppLink to={`/${HOME_PAGE}`}>
+        <FormattedMessage id="app.start_page" defaultMessage="site's start page" />
+      </AppLink>
     </h4>
   </div>
 )
