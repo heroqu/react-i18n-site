569a164162b2888d7609d5b121a4fa8d40762055
Heroqu <hero.qu@gmail.com>
2018-07-16 01:32:47 +0300

    add ScrollToTop to reset scroll position after navigation

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Route, Switch, Redirect } from 'react-router-dom'
 import { HOME_PAGE } from '../config'
 import LocaleRedirector from './LocaleRedirector'
+import ScrollToTop from './ScrollToTop'
 
 /**
  * App routing rules are here
  */
 const AppRouter = () => (
-  <Switch>
-    <Redirect exact from="/" to={`/${HOME_PAGE}`} />
-    <Route component={LocaleRedirector} />
-  </Switch>
+  <Fragment>
+    <ScrollToTop />
+    <Switch>
+      <Redirect exact from="/" to={`/${HOME_PAGE}`} />
+      <Route component={LocaleRedirector} />
+    </Switch>
+  </Fragment>
 )
 
 export default AppRouter
