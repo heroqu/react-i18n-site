a55c0084eb83531c57f851a0873b6cf2c4013ee7
Heroqu <hero.qu@gmail.com>
2018-07-17 22:05:57 +0300

    code prettifying

--- src/components/ResumeDownload/index.js
+++ src/components/ResumeDownload/index.js
@@ -7,6 +7,10 @@ import './ResumeDownload.css'
  * Incapsulate what exactly should happen when a user clicks on
  * `download resume` link
  */
-const ResumeDownload = ({ children }) => <A className="ResumeDownload" href="/data/resume.pdf">{children}</A>
+const ResumeDownload = ({ children }) => (
+  <A className="ResumeDownload" href="/data/resume.pdf">
+    {children}
+  </A>
+)
 
 export default ResumeDownload
