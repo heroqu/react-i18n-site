9230e947e8abb9196af426eaa1f0509692ec4124
Heroqu <hero.qu@gmail.com>
2018-07-18 06:54:48 -0400

    get rid of bindActionCreators as overkill

--- src/components/buttons/LocaleButton.js
+++ src/components/buttons/LocaleButton.js
@@ -1,19 +1,18 @@
 import React, { Fragment } from 'react'
 
-import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { setLocale } from '../../actions'
 
 import AppButton from './AppButton'
 
 /**
- * Link for switching the locale
+ * Button for switching the locale
  */
 const LocaleButton = ({ locale, toLocale, setLocale, children, ...rest }) => {
   const isActive = toLocale === locale
 
-  // allow to use explicit child element, but if absent,
-  // just fill in the locale value as text
+  // allow using explicit child element, but if absent,
+  // just fill in the target locale value as text
   if (!children) {
     children = <Fragment>{toLocale}</Fragment>
   }
@@ -33,7 +32,11 @@ const mapsStateToProps = state => ({
   locale: state.i18n.locale
 })
 
-const mapsDispatchToProps = dispatch =>
-  bindActionCreators({ setLocale }, dispatch)
+const mapDispatchToProps = dispatch => ({
+  setLocale: locale => dispatch(setLocale(locale))
+})
 
-export default connect(mapsStateToProps, mapsDispatchToProps)(LocaleButton)
+export default connect(
+  mapsStateToProps,
+  mapDispatchToProps
+)(LocaleButton)
