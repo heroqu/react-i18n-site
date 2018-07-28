d5746ba9a7bf6b7600b0a624ef64449778d7a709
Heroqu <hero.qu@gmail.com>
2018-07-28 10:53:05 -0400

    navMessages: rename & add more comments

--- src/Intl/navMessages.js
+++ src/Intl/navMessages.js
@@ -4,8 +4,34 @@ import { FormattedMessage } from 'react-intl'
 import { DEFAULT_LOCALE } from '../config'
 import messages from './messages.json'
 
+/**
+ * Only messages from default language branch
+ */
 const defaultMessages = messages[DEFAULT_LOCALE]
 
+/**
+ * Extract only those of them, that have keys like 'nav.something'.
+ *
+ * Example:
+ *
+ * {
+ *   en:
+ *    {
+ *      nav.intro: 'Intro',
+ *      nav.opensource: 'Open Source',
+ *      nav.about: 'About',
+ *      ...
+ *    },
+ *    ...
+ * }
+ *   ===>
+ * {
+ *   intro: 'Intro',
+ *   opensource: 'Open Source'
+ *   about: 'About'
+ *   ...
+ * }
+ */
 const defaultNavMessages = Object.keys(defaultMessages)
   .filter(key => key.split('.')[0] === 'nav')
   .reduce((acc, key) => {
@@ -13,29 +39,45 @@ const defaultNavMessages = Object.keys(defaultMessages)
     return acc
   }, {})
 
-// use unbreakable spaces inside menu titles
+/**
+ * Replace spaces with unbreakable spaces
+ * (we need that for menu titles)
+ */
 const unBreakSpace = txt => `${txt}`.replace(/ /g, '\u00a0')
 
+/**
+ * When making <FormattedMessage> the `id` key is not enough,
+ * as `defaultMessage` is also obligatory.
+ * Here we take care of automatically prepopulating defaultMessage attribute. *
+ * @param  {String} id          - message key
+ * @return {FormattedMessage}   - the component from React-intl package
+ */
+const makeFormattedMessage = id => {
+  let defaultMessage = (id && defaultMessages && defaultMessages[id]) || null
+  if (!defaultMessage) {
+    return null
+  }
+  defaultMessage = unBreakSpace(defaultMessage)
+  return <FormattedMessage {...{ id, defaultMessage }} />
+}
+
+/**
+ * For each key starting with `nav.` we create a <FormattedMessage>
+ * component in advance. This goes in the form of:
+ *
+ * {
+ *   intro: <FormattedMessage ... />,
+ *   about: <FormattedMessage ... />,
+ *   ...
+ * }
+ *
+ */
 export const navMessages = Object.keys(defaultNavMessages).reduce(
   (acc, attr) => {
-    acc[attr.toLowerCase()] = fmtMessage(`nav.${attr}`)
+    acc[attr.toLowerCase()] = makeFormattedMessage(`nav.${attr}`)
     return acc
   },
   {}
 )
-
-function fmtMessage(id) {
-  if (!id) return null
-
-  let defaultMessage = defaultMessages[id]
-
-  if (!defaultMessage) return null
-
-  if (defaultMessage.indexOf(' ') !== -1) {
-    defaultMessage = unBreakSpace(defaultMessage)
-  }
-
-  return <FormattedMessage {...{ id, defaultMessage }} />
-}
 
 export default navMessages
