90850089c06505e854d8d4c04d07f5e1cf1fa57d
Heroqu <hero.qu@gmail.com>
2018-07-29 17:00:26 -0400

    UL: commenting

--- src/components/UL.js
+++ src/components/UL.js
@@ -4,6 +4,9 @@ import classNames from 'classnames'
 /**
  * Add `List` class to the class names of the <ul>
  *
+ *   - just for the brevity at page authoring stage... and 
+ *   because we're not going to spoil classless <ul>
+ *   elements by applying some CSS to them.
  */
 const UL = ({ className, children, ...rest }) => (
   <ul className={classNames('List', className)} {...rest}>
