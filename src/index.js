33c5ef026a5a7b422e4053e59898271ad4526ad8
Heroqu <hero.qu@gmail.com>
2018-08-08 05:36:47 -0400

    rm service worker

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-import registerServiceWorker from './registerServiceWorker'
 
 import App from './App'
 
 render(<App />, document.getElementById('root'))
-
-registerServiceWorker()
